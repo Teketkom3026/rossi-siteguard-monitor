2b1115251ca539bd1dd6aebd64b46c56ebd292a4
Teketkom3026 <teketkom@yandex.ru>
2026-04-07 22:10:08 +0000

    fix: remove Hilt from Application class

--- siteguard-android/app/src/main/java/com/siteguard/monitor/SiteGuardApp.kt
+++ siteguard-android/app/src/main/java/com/siteguard/monitor/SiteGuardApp.kt
@@ -1,7 +1,14 @@
 package com.siteguard.monitor
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import com.siteguard.monitor.di.AppModule
+import com.siteguard.monitor.service.PushNotificationService
 
-@HiltAndroidApp
-class SiteGuardApp : Application()
+class SiteGuardApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppModule.init(this)
+        PushNotificationService.createNotificationChannel(this)
+    }
+}
+
